8ef8b15e1f588a5b7c0cd48c55cc1052425afd43
35efegunduz02 <114801781+35efegunduz02@users.noreply.github.com>
2022-10-15 08:58:34 +0300

    Create matematiksel_operatorler.py

---
+++ matematiksel_operatorler.py
@@ -0,0 +1,24 @@
+"""
+Toplama: +
+Çıkarma: -
+Çarpma: *
+Bölme: /
+Üs: **
+Mod: %
+Tam Bölme: //
+"""
+
+#toplama
+print(5487+9241)
+
+print(54-10)
+
+print(56*8)
+
+print(85/5)
+
+print(3**4)
+
+print(10%90)
+
+print(1090//100)
