d90e12fae74ff3c0236efcbb61136f14bce36262
35efegunduz02 <114801781+35efegunduz02@users.noreply.github.com>
2022-10-22 11:23:06 +0300

    Create string_kacis_dizileri.py

---
+++ string_kacis_dizileri.py
@@ -0,0 +1,36 @@
+"""
+Python’ın gözünde bir başlangıç tırnağı ile bitiş tırnağı arasında yer alan
+her şey bir karakter dizisidir. Ancak bizler metin içerisinde bu tırnakları
+özel bir amaçla kullanmak isteyebiliriz. Bu tür durumlarda kaçış karakteri
+olan ters bölü işaretini (\) kullanarak hatadan kaçarız.
+"""
+
+print("ahmet, \"bugün sinemaya gidiyorum.\" dedi.")
+
+print("yarın adana\'ya gidiyorum.")
+
+
+# özel karakterler
+
+print("Efe\ngündüz") #alt satır
+print("efe\tgündüz") #tab
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
